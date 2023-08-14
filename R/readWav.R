bf0485880de6b1147b16aa086eaba2d30dea2dcd
Jan Brønd <jcb@video4coach.com>
2023-08-14 15:54:53 +0200

    Update readWav.R

--- R/readWav.R
+++ R/readWav.R
@@ -20,20 +20,22 @@ readWav = function(filename, start = 1, end = 100, units = "minutes") {
       rownames(header) <- header[,1]
       
       
-      }, silent = TRUE)
-    if (length(header) == 1) {
+      
+    #if (length(header) == 1) {
       #header = rownames(read.csv(filename, skipNul = TRUE, nrow = Nlines, header = TRUE, fileEncoding = "WINDOWS-1252"))
-      header = read.csv(filename, nrow = Nlines,  skipNul = TRUE, header = FALSE, fileEncoding = "WINDOWS-1252")
-      rownames(header) <- header[,1]
-    }
-    if (length(header) == 1) {
+    #  header = read.csv(filename, nrow = Nlines,  skipNul = TRUE, header = FALSE, fileEncoding = "WINDOWS-1252")
+    #  rownames(header) <- header[,1]
+    #}
+    #if (length(header) == 1) {
       #header = rownames(read.csv(filename, skipNul = TRUE, nrow = Nlines, header = TRUE, fileEncoding = "UTF-8"))
-      header = read.csv(filename, nrow = Nlines,  skipNul = TRUE, header = FALSE, fileEncoding = "UTF-8")
-      rownames(header) <- header[,1]
-    }
+    #  header = read.csv(filename, nrow = Nlines,  skipNul = TRUE, header = FALSE, fileEncoding = "UTF-8")
+    #  rownames(header) <- header[,1]
+    #}
     #if (length(header) == 0) {
     #  header = rownames(read.csv(filename, skipNul = TRUE, nrow = Nlines, header = TRUE, fileEncoding = "latin1"))
     #}
+    
+    }, silent = TRUE)
     
     Nlines = Nlines - 1
     if (Nlines == 10) print("Error: wav file header not recognized in function g.wavread")
